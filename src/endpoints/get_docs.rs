46df62c0a8e0c0c1a2b053dbc09fee1ff5438178
Eduardo Loback <eduardolloback@gmail.com>
2024-04-23 09:11:18 -0300

    feat: list docs route

---
+++ src/endpoints/get_docs.rs
@@ -0,0 +1,48 @@
+use derive_builder::Builder;
+use rustify::Wrapper;
+use rustify_derive::Endpoint;
+use serde::de::DeserializeOwned;
+use serde_derive::{Deserialize, Serialize};
+use serde_json::Value;
+
+pub type DocsResponse = Vec<Document>;
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Document {
+    #[serde(rename = "open_id")]
+    pub open_id: i64,
+    pub token: String,
+    pub status: String,
+    pub name: String,
+    #[serde(rename = "original_file")]
+    pub original_file: String,
+    #[serde(rename = "signed_file")]
+    pub signed_file: Value,
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+    #[serde(rename = "last_update_at")]
+    pub last_update_at: String,
+}
+
+// Defines an API endpoint at /test/path that takes no inputs and returns an
+// empty response.
+#[derive(Builder, Default, Endpoint, Serialize)]
+#[endpoint(path = "docs", response = "DocsResponse", builder = "true")]
+#[builder(setter(into, strip_option), default)]
+pub struct Docs {
+    #[endpoint(query = "page")]
+    page: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PaginationWrapper<T> {
+    pub count: i32,
+    pub next: Option<String>,
+    pub previous: Option<String>,
+    pub results: T,
+}
+
+impl<T: DeserializeOwned + Send + Sync> Wrapper for PaginationWrapper<T> {
+    type Value = T;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod endpoints;
+
+pub use endpoints::*;
+pub use rustify::{errors::ClientError, Client, Endpoint};
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{Client, ClientError, Endpoint};
+    use crate::get_docs::{Docs, PaginationWrapper};
+    const API_TOKEN: &str = std::env!("API_TOKEN");
+    const BASE_URL: &str = std::env!("BASE_URL");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        let endpoint = Docs::builder().page(1).build().unwrap();
+        // client with authentication token
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(reqwest::header::AUTHORIZATION, API_TOKEN.parse().unwrap());
+        headers.insert(
+            reqwest::header::CONTENT_TYPE,
+            "application/json".parse().unwrap(),
+        );
+        let req_client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let client = Client::new(BASE_URL, req_client); // Configures base address of http://api.com
+
+        let result = endpoint.exec(&client).await; // Sends GET request to http://api.com/test/path
+        match result {
+            Ok(r) => match r.wrap::<PaginationWrapper<_>>() {
+                Ok(d) => {
+                    d.results.iter().for_each(|d| println!("{:#?}", d));
+                }
+                Err(ClientError::ResponseParseError { source, content }) => {
+                    println!("{}: {}", source, content.unwrap_or("".into()))
+                }
+                r => println!("{:#?}", r),
+            },
+            Err(e) => match e {
+                _ => println!("{:#?}", e),
+            },
+        };
     }
 }
