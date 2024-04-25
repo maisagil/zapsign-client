7ac7bf187f1c26ab458c59f3d251cddbfbc27002
Eduardo Loback <eduardolloback@gmail.com>
2024-04-25 16:24:28 -0300

    feat: wiremock example

--- src/endpoints/get_docs.rs
+++ src/endpoints/get_docs.rs
@@ -3,11 +3,11 @@ use rustify::Wrapper;
 use rustify_derive::Endpoint;
 use serde::de::DeserializeOwned;
 use serde_derive::{Deserialize, Serialize};
-use serde_json::Value;
 
 pub type DocsResponse = Vec<Document>;
 
-#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[cfg_attr(test, derive(Eq))]
 #[serde(rename_all = "camelCase")]
 pub struct Document {
     #[serde(rename = "open_id")]
@@ -18,7 +18,7 @@ pub struct Document {
     #[serde(rename = "original_file")]
     pub original_file: String,
     #[serde(rename = "signed_file")]
-    pub signed_file: Value,
+    pub signed_file: Option<String>,
     #[serde(rename = "created_at")]
     pub created_at: String,
     #[serde(rename = "last_update_at")]
@@ -35,7 +35,8 @@ pub struct Docs {
     page: i32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Default, Debug, Deserialize, Serialize)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct PaginationWrapper<T> {
     pub count: i32,
     pub next: Option<String>,
--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,30 @@ pub use rustify::{errors::ClientError, Client, Endpoint};
 
 #[cfg(test)]
 mod tests {
-    use super::{Client, ClientError, Endpoint};
-    use crate::get_docs::{Docs, PaginationWrapper};
+    use super::{Client, Endpoint};
+    use crate::get_docs::{Docs, Document, PaginationWrapper};
+    use wiremock::matchers::{header, method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
     const API_TOKEN: &str = std::env!("API_TOKEN");
-    const BASE_URL: &str = std::env!("BASE_URL");
+    // const BASE_URL: &str = std::env!("BASE_URL");
 
     #[tokio::test]
     async fn it_works() {
+        let mock_server = MockServer::start().await;
+
+        let mock_response = PaginationWrapper {
+            results: vec![Document::default()],
+            ..Default::default()
+        };
+
+        Mock::given(method("GET"))
+            .and(path("/docs"))
+            .and(header("Authorization", API_TOKEN))
+            .respond_with(ResponseTemplate::new(200).set_body_json(&mock_response))
+            .mount(&mock_server)
+            .await;
+
         let endpoint = Docs::builder().page(1).build().unwrap();
         // client with authentication token
         let mut headers = reqwest::header::HeaderMap::new();
@@ -25,22 +42,25 @@ mod tests {
             .build()
             .unwrap();
 
-        let client = Client::new(BASE_URL, req_client); // Configures base address of http://api.com
+        let client = Client::new(&mock_server.uri(), req_client); // Configures base address of http://api.com
 
         let result = endpoint.exec(&client).await; // Sends GET request to http://api.com/test/path
-        match result {
-            Ok(r) => match r.wrap::<PaginationWrapper<_>>() {
-                Ok(d) => {
-                    d.results.iter().for_each(|d| println!("{:#?}", d));
-                }
-                Err(ClientError::ResponseParseError { source, content }) => {
-                    println!("{}: {}", source, content.unwrap_or("".into()))
-                }
-                r => println!("{:#?}", r),
-            },
-            Err(e) => match e {
-                _ => println!("{:#?}", e),
-            },
-        };
+        assert!(result.is_ok());
+        let result = result.unwrap().wrap::<PaginationWrapper<_>>().unwrap();
+        assert_eq!(mock_response, result);
+        // match result {
+        //     Ok(r) => match r.wrap::<PaginationWrapper<_>>() {
+        //         Ok(d) => {
+        //             d.results.iter().for_each(|d| println!("{:#?}", d));
+        //         }
+        //         Err(ClientError::ResponseParseError { source, content }) => {
+        //             println!("{}: {}", source, content.unwrap_or("".into()))
+        //         }
+        //         r => println!("{:#?}", r),
+        //     },
+        //     Err(e) => match e {
+        //         _ => println!("{:#?}", e),
+        //     },
+        // };
     }
 }
